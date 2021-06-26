c60c28edd6c12c9bddaee8709c85e8e4ffddd080
Rohit Gohri <work@rohit.page>
2021-06-26 21:46:19 +0530

    fix import

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import github from '@actions/github'
+import * as github from '@actions/github'
 import createJiraAPI from './jira/api'
 
 async function run(): Promise<void> {
