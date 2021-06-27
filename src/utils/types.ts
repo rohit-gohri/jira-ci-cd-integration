9d08b8790478dd4a71f5d9c056504a139aeb7720
Rohit Gohri <work@rohit.page>
2021-06-27 08:33:35 +0530

    🚨 disable any rule

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,6 @@
 export type Unpromise<T> = T extends Promise<infer U> ? U : T
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ReturnTypeResolved<T extends (...args: any) => any> = Unpromise<
   ReturnType<T>
 >
